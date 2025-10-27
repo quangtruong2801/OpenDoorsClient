b227dee11739af030e556a7531c5c3f3e687c8b7
quangtruong2801 <truongtruong2801@gmail.com>
2025-10-27 11:15:31 +0700

    Nested-Routes

--- src/layouts/PrivateRoute.tsx
+++ src/layouts/PrivateRoute.tsx
@@ -1,28 +1,24 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/useAuth";
 
 interface PrivateRouterProps {
-  children: JSX.Element;
   adminOnly?: boolean;
 }
 
-export default function PrivateRouter({ children, adminOnly = false }: PrivateRouterProps) {
+export default function PrivateRouter({ adminOnly = false }: PrivateRouterProps) {
   const { user, loading } = useAuth();
 
-  // Khi đang loading thì hiển thị màn hình chờ
   if (loading) {
     return <div className="text-center p-10">🔄 Đang xác thực người dùng...</div>;
   }
 
-  // Nếu chưa đăng nhập
   if (!user) {
     return <Navigate to="/recruitments" replace />;
   }
 
-  // Nếu yêu cầu admin mà user không phải admin
   if (adminOnly && user.role !== "admin") {
     return <Navigate to="/recruitments" replace />;
   }
 
-  return children;
+  return <Outlet />; 
 }
